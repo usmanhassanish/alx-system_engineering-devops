d5d818428a27343c0ee78b04da48651e802db973
ussyneh <usmanhassanish@gmail.com>
2023-09-22 07:46:59 -0700

    updated task 0

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# create a file with permissions and a content
+file { '/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet', # This is another comment
+}
